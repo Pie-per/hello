2b2288bcd8b9c10751eb250e306b6b6e89af33e2
Pie-per <82899627+Pie-per@users.noreply.github.com>
2021-05-15 07:27:06 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,54 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
+    Level2()
+})
+function Level2 () {
+    tiles.setTilemap(tilemap`level0`)
+    mySprite.setPosition(30, 190)
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy += -250
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    Level2()
+})
+function Level1 () {
+    tiles.setTilemap(tilemap`level2`)
+    mySprite.setPosition(20, 190)
+}
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    ...............
+    .....55555.....
+    ....5ddddd5....
+    ...5ddddddd5...
+    ...5d11d11d5...
+    ...5d61d61d5...
+    ...5ddddddd5...
+    ...5dd33ddd5...
+    ...55ddddd55...
+    ...5551d1555...
+    ...5591d1955...
+    ...55d111d55...
+    ...55d111d55...
+    .....d111d.....
+    .....d111d.....
+    ....9d111d9....
+    ...91d111d19...
+    ..911d111d119..
+    ..91111111119..
+    ..91111111119..
+    .9911111111199.
+    .9911111111199.
+    .9911111111199.
+    .9911111111199.
+    .9911111111199.
+    .9911111111199.
+    .9999999999999.
+    ...............
     `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+mySprite.ay = 500
+Level1()
