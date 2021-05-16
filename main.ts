99c6d1558740e987e84c6cbe6d878200d739b328
Pie-per <82899627+Pie-per@users.noreply.github.com>
2021-05-16 13:41:51 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,43 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
-    Level2()
+    Level3()
 })
 function Level2 () {
-    tiles.setTilemap(tilemap`level0`)
-    mySprite.setPosition(30, 190)
+    tiles.setTilemap(tilemap`level7`)
+    mySprite.setPosition(20, 180)
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy += -250
+        mySprite.vy += -300
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
+    game.over(true)
+})
+function Level3 () {
+    tiles.setTilemap(tilemap`level2`)
+    mySprite.setPosition(20, 180)
+    for (let index = 0; index < 20; index++) {
+        MyEnemy = sprites.createProjectileFromSide(assets.tile`myTile8`, 0, 50)
+        MyEnemy.x = randint(5, 155)
+        MyEnemy.setKind(SpriteKind.Enemy)
+        pause(100)
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     Level2()
 })
 function Level1 () {
-    tiles.setTilemap(tilemap`level2`)
+    tiles.setTilemap(tilemap`level0`)
     mySprite.setPosition(20, 190)
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile16, function (sprite, location) {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.spray, 500)
+})
+let MyEnemy: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     ...............
